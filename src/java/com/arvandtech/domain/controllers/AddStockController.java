ff0a83c3fea9ac93042b294b0f831a80883c1fa6
User <User@PC3.Arvandtech.local>
2018-06-04 13:47:11 +1000

    Added Selection with 2 boxes and 1. Does not function yet.

--- src/java/com/arvandtech/domain/controllers/AddStockController.java
+++ src/java/com/arvandtech/domain/controllers/AddStockController.java
@@ -9,8 +9,12 @@ import com.arvandtech.domain.Attribute;
 import com.arvandtech.domain.ItemType;
 import com.arvandtech.domain.ItemTypeFacade;
 import com.arvandtech.domain.SelectableBox;
+import com.arvandtech.domain.ItemAttribute;
+import com.arvandtech.domain.SecondaryAttribute;
+import com.arvandtech.domain.SelectableBoxFacade;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -28,8 +32,12 @@ public class AddStockController implements Serializable {
     @EJB
     private ItemTypeFacade itemFacade;
 
+    @EJB
+    private SelectableBoxFacade selectFacade;
+
     private ItemType item;
-    private ArrayList<Boolean> attExists;
+    private ArrayList<ItemAttribute> attributes;
+    private ArrayList<SelectableBox> selections;
     private String tmpEntry;
 
     public List<ItemType> findAllItems() {
@@ -37,14 +45,15 @@ public class AddStockController implements Serializable {
     }
 
     public boolean checkAttributeExist() {
-        attExists = new ArrayList<>(11);
+        attributes = new ArrayList<>();
+        selections = new ArrayList<>();
         try {
             for (int i = 0; i < 10; i++) {
                 try {
                     item.getAttribute().get(i);
-                    attExists.add(true);
+                    attributes.add(new ItemAttribute());
+                    selections.add(new SelectableBox());
                 } catch (Exception e) {
-                    attExists.add(false);
                 }
             }
             return true;
@@ -53,28 +62,48 @@ public class AddStockController implements Serializable {
         }
     }
 
-    public String findTitle(int i)
-    {
-        return item.getAttribute().get(i-1).getAttributeName();
+    public String findTitle(int i) {
+        return item.getAttribute().get(i - 1).getAttributeName();
     }
-    
+
+    public String findSecondaryTitle(int i) {
+        return attributes.get(i - 1).getSecondaryName();
+    }
+
     public void clearItem() {
         item = new ItemType();
     }
 
     public boolean attributeExists(int i) {
-        try {
-            item.getAttribute().get(i - 1);
+        if (i <= attributes.size()) {
             return true;
-        } catch (Exception e) {
+        } else {
             return false;
         }
     }
 
+    public void updatePrimaryAttribute(int i, String select) {
+        SelectableBox tmpSelect = selectFacade.find(Integer.valueOf(select));
+        ItemAttribute tmpAtt = new ItemAttribute();
+        tmpAtt.setAttributeName(item.getAttribute().get(i - 1).getAttributeName());
+        tmpAtt.setAttributeValue(tmpSelect.getName());
+        if (tmpSelect.isSecondary()) {
+            tmpAtt.setSecondaryName(tmpSelect.getSecondaryName());
+        }
+        attributes.set(i - 1, tmpAtt);
+        selections.set(i - 1, tmpSelect);
+    }
+
     public String findLink(int i) {
-        if (attExists.get(i-1)) {
+        if (attributeExists(i)) {
             Attribute tmpAtt = item.getAttribute().get(i - 1);
             if (tmpAtt.isSelectable()) {
+                try {
+                    if (selections.get(i - 1).getName() != null && selections.get(i - 1).isSecondary()) {
+                        return "layouts/twoSelections.xhtml";
+                    }
+                } catch (Exception e) {
+                }
                 return "layouts/oneSelection.xhtml";
             } else {
                 return "layouts/entry.xhtml";
@@ -82,10 +111,16 @@ public class AddStockController implements Serializable {
         }
         return "";
     }
-    
+
     public List<SelectableBox> findSelectables(int i) {
-            Attribute tmpAtt = item.getAttribute().get(i - 1);
-            return tmpAtt.getSelectableBox();
+        Attribute tmpAtt = item.getAttribute().get(i - 1);
+        tmpAtt.getSelectableBox().sort(Comparator.comparing(SelectableBox::getSelectableOrder));
+        return tmpAtt.getSelectableBox();
+    }
+
+    public List<SecondaryAttribute> findSecondaries(int i) {
+        selections.get(i-1).getSecondaryAttribute().sort(Comparator.comparing(SecondaryAttribute::getSecondaryOrder));
+        return selections.get(i-1).getSecondaryAttribute();
     }
 
     //GETTERS
